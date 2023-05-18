cc1fccb75bb5a36b69599c8c0e32ed2b3588a62c
Kuldeep Pal <33137390+universeservices@users.noreply.github.com>
2023-05-19 01:15:40 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.post("/predict/")
+def predict():
+    # Code to handle the prediction logic using TahaDouaji/detr-doc-table-detection model
+    # ...
+    # Return the prediction response
+    return {"result": "Prediction result"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
